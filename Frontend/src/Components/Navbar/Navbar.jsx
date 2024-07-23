050bf5a1a3a36bd8bdc2fedfc143c5bf40619a50
Vijay Krishan <vk856549@gmail.com>
2024-07-23 23:04:11 +0530

    initial commit

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import '../Navbar/Navbar.css';
+
+import { IoClose } from "react-icons/io5";
+import { RxDropdownMenu } from "react-icons/rx";
+
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-        <div>Logo</div>
-        <ul>
-            <li>
-                <Link>Home</Link>
-            </li>
-            <li>
-            <Link>services</Link>
-            </li>
-            <li>
-            <Link>About me</Link>
-            </li>
+    <div className="container w-full mx-auto flex flex-row m-4 h-14 justify-between items-center ">
+      <div className=""><img src="/src/assets/v (2).png" className="h-20" alt="" /></div>
+      <div className="flex nav">
+        <ul className={` flex gap-7 items-center justify-center ${isMenuOpen ? 'nav-link show' : 'nav-link'}`}>
+          <li>
+            <NavLink to='/home' activeClassName="active">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/services' activeClassName="active">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to='/portfolio' activeClassName="active">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to='/contact' activeClassName="active">Contact</NavLink>
+          </li>
         </ul>
+        <div className="menu text-3xl" onClick={toggleMenu}>
+          {isMenuOpen ? <IoClose /> : <RxDropdownMenu />}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
