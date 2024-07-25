5f1425f9be9d6bbf088e3639638c8f4cd226d383
Vijay Krishan <vk856549@gmail.com>
2024-07-25 23:34:12 +0530

    initial commit

---
+++ Frontend/src/Route.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+export default function Route() {
+  return (
+    <main>
+      <header>
+        <Navbar />
+      </header>
+      <Outlet />
+      <footer>
+        
+      </footer>
+    </main>
+  );
+}
