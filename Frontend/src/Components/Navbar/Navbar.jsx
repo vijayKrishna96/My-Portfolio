fd8e038e7ecbe2b8ed348e8ab535b5fc1e882cd5
Vijay Krishan <vk856549@gmail.com>
2024-07-26 23:16:36 +0530

    navbar edited

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import '../Navbar/Navbar.css';
+import "../Navbar/Navbar.css";
 
 import { IoClose } from "react-icons/io5";
 import { RxDropdownMenu } from "react-icons/rx";
-
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,25 +13,45 @@ export default function Navbar() {
   };
 
   return (
-    <div className="container w-full mx-auto flex flex-row m-4 h-14 justify-between items-center ">
-      <div className=""><img src="/src/assets/v (2).png" className="h-12 rounded-full" alt="" /></div>
-      <div className="flex nav">
-        <ul className={` flex gap-7 items-center justify-center text-white ${isMenuOpen ? 'nav-link show' : 'nav-link'}`}>
-          <li>
-            <NavLink to='/home' activeClassName="active ">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to='/services' activeClassName="active">Services</NavLink>
-          </li>
-          <li>
-            <NavLink to='/portfolio' activeClassName="active">Portfolio</NavLink>
-          </li>
-          <li>
-            <NavLink to='/contact' activeClassName="active">Contact</NavLink>
-          </li>
-        </ul>
-        <div className="menu text-3xl text-white" onClick={toggleMenu}>
-          {isMenuOpen ? <IoClose /> : <RxDropdownMenu />}
+    <div className="container  mx-auto">
+      <div className="fixed flex container mx-auto flex-row pt-6 h-14 justify-between items-center">
+        <div className="">
+          <img
+            src="/src/assets/v (2).png"
+            className="h-12 rounded-full"
+            alt=""
+          />
+        </div>
+        <div className="flex nav">
+          <ul
+            className={` flex gap-7 items-center justify-center text-white ${
+              isMenuOpen ? "nav-link show" : "nav-link"
+            }`}
+          >
+            <li>
+              <NavLink to="/home" activeClassName="active ">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services" activeClassName="active">
+                Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/portfolio" activeClassName="active">
+                Portfolio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" activeClassName="active">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <div className="menu text-3xl text-white" onClick={toggleMenu}>
+            {isMenuOpen ? <IoClose /> : <RxDropdownMenu />}
+          </div>
         </div>
       </div>
     </div>
